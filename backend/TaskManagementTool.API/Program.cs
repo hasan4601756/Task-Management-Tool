a9d693d8e757feac32116a22ed5dc910d8c19f3c
Muhammad Ahmed Hasan <ahmedhasan.2k5@gmail.com>
2026-01-11 22:59:39 +0500

    Revert "Miscellaneous files commited"
    This reverts commit 1a1207e8d50feaf868e507ca7d0c12e62a2690d6, reversing
    changes made to a049516625a7443d388d4b6152d6232a3b2cc203.

--- backend/TaskManagementTool.API/Program.cs
+++ backend/TaskManagementTool.API/Program.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using Microsoft.AspNetCore.Identity;
 using TaskManagementTool.Domain.Enums;
 using TaskManagementTool.Infrasrtucure.Identity;
@@ -26,46 +25,3 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.Run();
-=======
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
->>>>>>> 19b017718179b8e80ed796116164f20c8740f169
