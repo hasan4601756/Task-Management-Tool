8d91b51b0f24c0e0a7b075ddaeced7e11eb612a6
Muhammad Ahmed Hasan <ahmedhasan.2k5@gmail.com>
2026-01-13 19:08:52 +0500

    Apllied Migrations and Updated Database

--- backend/TaskManagementTool.API/Program.cs
+++ backend/TaskManagementTool.API/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using TaskManagementTool.Domain.Enums;
 using TaskManagementTool.Infrasrtucure.Identity;
 using TaskManagementTool.Infrastructure.Data;
@@ -6,6 +7,12 @@ using TaskManagementTool.Infrastructure.Identity;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+builder.Services
+    .AddDbContext<AppDbContext>(
+        options => options.UseSqlServer(
+            builder.Configuration.GetConnectionString("DefaultConnection")
+        )
+    );
 builder.Services
     .AddIdentity<ApplicationUser, ApplicationRole>(options =>
     {
